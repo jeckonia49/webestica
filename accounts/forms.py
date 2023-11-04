e6b799efa24175a80838fcc7ccb701f20dd932d4
django unleash <ckwriter72@gmail.com>
2023-11-04 03:03:45 +0300

    minor chnage

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,17 @@
+from collections.abc import Mapping
+from typing import Any
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
+from django import forms
+from django.forms.utils import ErrorList
 from .models import Account
+
+
+class FormSetting(forms.Form):
+    def __init__(self,  *args, **kwargs):
+        super(FormSetting, self).__init__(*args, **kwargs)
+        for field in self.visible_fields():
+            field.field.widget.attrs['class'] = 'form-control'
+
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -13,3 +24,13 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = Account
         fields = ("email",)
+
+class LoginForm(FormSetting):
+    class Meta:
+        fields = ['email', 'password']
+
+    widgets = {
+        "email": forms.EmailInput(attrs={"placeholder":"Email Address", "id": "exampleInputEmail1"}),
+        "password": forms.PasswordInput(attrs={"placeholder":"Password", "id": "exampleInputPassword1"})
+    }
+
